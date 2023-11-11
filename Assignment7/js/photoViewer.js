80f4f7d816a10fe4bd17fe3c9eebe318d2555a7f
lovejotsingh06 <w0829892@myscc.ca>
2023-11-10 19:03:14 -0500

    assignment7

---
+++ Assignment7/js/photoViewer.js
@@ -0,0 +1,8 @@
+(function ($) {
+    $.fn.customPhotoViewer = function () {
+        var $photoViewer = this;
+        var $thumbnails = $photoViewer.find('.thumbnail-anchor');
+
+        return this;
+    };
+})(jQuery);
---
+++ Assignment7/js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('#photo-viewer').customPhotoViewer().show().on('click', '.photo-box', function (e) 
+    {
+        var $content = $(this).clone().find('img').css({
+            marginLeft: 0,
+            marginTop: 0,
+            width: '100%',
+            height: 'auto'
+        });
+      
+    });
+});
